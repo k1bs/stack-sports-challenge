bede0907e75756bef415004691942ec4070b14ad
Alex Kibler <alexkibler@me.com>
2018-07-14 09:59:10 -0600

    remove scaffolding from App.js, create basic RosterContainer.js

---
+++ src/components/RosterContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RosterContainer extends Component {
+  render () {
+    return (
+      <p>Roster Container</p>
+    )
+  }
+}
+
+export default RosterContainer

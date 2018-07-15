4349a6435ec91e89debc6aefbbc1098f9bd61654
Alex Kibler <alexkibler@me.com>
2018-07-15 17:25:54 -0600

    add bulma css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Header from './components/Header.jsx'
 import RosterContainer from './components/RosterContainer.jsx'
 import './App.css'
 
@@ -6,6 +7,7 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
+        <Header />
         <RosterContainer />
       </div>
     )

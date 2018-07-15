a27de32405cdc3c0a5af9a451c45dcef5aed11ec
Alex Kibler <alexkibler@me.com>
2018-07-15 17:27:00 -0600

    add basic header, import

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <section className='hero'>
+      <div className='hero-body'>
+        <div className='container'>
+          <h1 className='title'>
+            Team Name goes here
+          </h1>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Header

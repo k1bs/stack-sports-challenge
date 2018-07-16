254a31178a0e70639fcee81493dc9493d46c9c45
Alex Kibler <alexkibler@me.com>
2018-07-15 21:40:43 -0600

    add styling to form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Landing from './components/Landing'
 import Footer from './components/Footer'
 import RosterContainer from './components/RosterContainer'
+import './App.css'
 
 class App extends Component {
   constructor () {
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -20,11 +20,18 @@ class Landing extends Component {
     return (
       <section className='hero is-primary is-bold is-fullheight'>
         <div className='hero-body'>
-          <div className='container'>
+          <div className='container name-container'>
             <form onSubmit={(event) => this.props.handleRosterNameSubmit(event, this.state.teamName)}>
               <div className='field'>
                 <label className='label is-large has-text-white'>Name your Team</label>
-                <input class='input is-large' type='text' onChange={this.handleInputChange} value={this.state.teamName} placeholder='Your Team Name' />
+                <div className='control'>
+                  <input class='input is-large' type='text' onChange={this.handleInputChange} value={this.state.teamName} placeholder='Your Team Name' />
+                </div>
+              </div>
+              <div className='field'>
+                <div className='control'>
+                  <button className='button is-link is-large'>Submit</button>
+                </div>
               </div>
             </form>
           </div>

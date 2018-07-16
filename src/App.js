f058379914fb3ea4e3c9c71efb583a2311d0265a
Alex Kibler <alexkibler@me.com>
2018-07-16 12:41:05 -0600

    remove default state

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      rosterName: 'farts',
+      rosterName: '',
       namingMode: true
     }
     this.handleRosterNameSubmit = this.handleRosterNameSubmit.bind(this)

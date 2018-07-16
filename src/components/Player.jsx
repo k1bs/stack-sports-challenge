addf1e96f6e1d0a3198cda3d542126b360259004
Alex Kibler <alexkibler@me.com>
2018-07-15 18:31:02 -0600

    Add basic player data

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Player = (props) => {
   return (
-    <div className='tile is-child box'>
-      <p>{props.player.firstName}</p>
-    </div>
+    <article className='tile is-child box'>
+      <h5 className='title is-5'>{props.player.firstName} {props.player.lastName}</h5>
+      <h5 className='subtitle is-5'>{props.player.firstName} {props.player.lastName}</h5>
+      <p>Strength: {props.player.strength}</p>
+      <p>Speed: {props.player.speed}</p>
+      <p>Agility: {props.player.agility}</p>
+    </article>
   )
 }
 

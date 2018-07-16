d8eea4e73069ac7cbb7406882575b933e12c9ad0
Alex Kibler <alexkibler@me.com>
2018-07-15 18:08:10 -0600

    add basic footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = (props) => {
+  return (
+    <footer className='footer'>
+      <div className='content has-text-centered'>
+        <p>You should hire Alex</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer

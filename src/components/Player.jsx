7bb5d21e3204aa67aa83f6c184df593bea9e3836
Alex Kibler <alexkibler@me.com>
2018-07-16 07:48:35 -0600

    convert player to class component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Player = (props) => {
-  return (
-    <article className='is-child box'>
-      <h4 className='title is-4'>{props.player.firstName} {props.player.lastName}</h4>
-      <h6 className='subtitle is-6'>{props.player.alphaNumeric}</h6>
-      <p>Strength: {props.player.scores.strength}</p>
-      <p>Speed: {props.player.scores.speed}</p>
-      <p>Agility: {props.player.scores.agility}</p>
-    </article>
-  )
+class Player extends Component {
+  constructor (props) {
+    super(props)
+  }
+  render () {
+    return (
+      <article className='box'>
+        <h4 className='title is-4'>{this.props.player.firstName} {this.props.player.lastName}</h4>
+        <h6 className='subtitle is-6'>{this.props.player.alphaNumeric}</h6>
+        <p>Strength: {this.props.player.scores.strength}</p>
+        <p>Speed: {this.props.player.scores.speed}</p>
+        <p>Agility: {this.props.player.scores.agility}</p>
+      </article>
+    )
+  }
 }
 
 export default Player

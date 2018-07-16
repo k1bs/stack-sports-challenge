21c5182aef994bf45630df08cc095a5310748ea0
Alex Kibler <alexkibler@me.com>
2018-07-15 19:07:24 -0600

    add nameGenerator method, recursively ensure no duplicate names

--- src/components/RosterContainer.jsx
+++ src/components/RosterContainer.jsx
@@ -7,27 +7,52 @@ class RosterContainer extends Component {
     super()
     this.state = {
       rosterName: '',
-      roster: []
+      roster: [],
+      usedNames: [],
+      usedScores: []
     }
   }
 
   componentWillMount () {
+    this.generateRoster()
+  }
+
+  generateRoster () {
     let newRoster = []
     for (let i = 0; i < 15; i++) {
       let startingStatus = i < 10
       newRoster.push({
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
+        firstName: this.nameGenerator('first'),
+        lastName: this.nameGenerator('last'),
         speed: Math.round((Math.random() * 32) + 1),
         strength: Math.round((Math.random() * 32) + 1),
         agility: Math.round((Math.random() * 32) + 1),
         startingStatus
       })
     }
-    console.log(newRoster)
     this.setState({
       roster: newRoster
     })
+  }
+
+  nameGenerator (type) {
+    let name
+    switch (type) {
+      case 'first':
+        name = faker.name.firstName()
+        break
+      case 'last':
+        name = faker.name.lastName()
+        break
+    }
+    if (!this.state.usedNames.includes(name)) {
+      this.setState({
+        usedNames: [...this.state.usedNames, name]
+      })
+      return name
+    } else {
+      this.nameGenerator(type)
+    }
   }
 
   renderStarters () {

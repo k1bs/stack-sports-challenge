9c6a3ad59cd64ce3eed602facdeaccae0a77d014
Alex Kibler <alexkibler@me.com>
2018-07-15 18:08:30 -0600

    add styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 const Header = (props) => {
   return (
-    <section className='hero'>
+    <section className='hero is-primary is-bold'>
       <div className='hero-body'>
         <div className='container'>
           <h1 className='title'>
             Team Name goes here
           </h1>
+          <p className='subtitle'>Home of Champions</p>
         </div>
       </div>
     </section>
